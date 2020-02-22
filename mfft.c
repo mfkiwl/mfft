a01aa48e77c54f12a02fe9a24142efe282ea9574
WenZheng Li <me@wenzheng.li>
2020-02-22 16:57:00 +0900

    polished fft code

--- mfft.c
+++ mfft.c
@@ -1,24 +1,24 @@
-#include <complex.h>
-
 #include "mfft.h"
+#include <assert.h>
 
 static void bit_reverse_permutation(float complex* data, int len, int stride) {
-    for (int i = 0, target = 0; i < len; ++i) {                       
-        if (target > i) {                                                            
+    for (int i = 0, target = 0; i < len; ++i) {
+        if (target > i) {
             float complex tmp = data[i * stride];
             data[i * stride] = data[target * stride];
             data[target * stride] = tmp;
-        }                                                                                
-        int mask = len;                                                            
-        while (target & (mask >>= 1)) {                                                  
-            target &= ~mask;                                                             
-        }                                                                                
-        target |= mask;                                                                  
+        }
+        int mask = len;
+        while (target & (mask >>= 1)) {
+            target &= ~mask;
+        }
+        target |= mask;
     }
 }
 
-static void fast_fourier_transform_butterfly_group(float complex* data, int len, int stride, int stage, int group, float complex twiddle_factor) { 
-    for (int pair = group; pair < len; pair += 2 * stage) {                 
+static void compute_butterfly_for_group(float complex* data, int len, int stride, int stage,
+        int group, float complex twiddle_factor) {
+    for (int pair = group; pair < len; pair += 2 * stage) {
         data[(pair + stage) * stride] *= -twiddle_factor;
         data[(pair + stage) * stride] += data[pair * stride];
         data[pair * stride] += data[pair * stride];
@@ -26,23 +26,29 @@ static void fast_fourier_transform_butterfly_group(float complex* data, int len,
     }
 }
 
-static void fast_fourier_transform(float complex* data, int len, int stride, int inverse) {                                                                                        
-    bit_reverse_permutation(data, len, stride);                                             
-    fast_fourier_transform_butterfly_group(data, len, stride, 1, 0, 1); 
-    float complex rotation = (inverse > 0) ? I : -I;                                         
+void fast_fourier_transform(float complex* data, int len, int stride, int inverse) {
+    assert(data && len >= 0 && IS_POWER_OF_TWO(len) && stride >= 0);
+    bit_reverse_permutation(data, len, stride);
+    compute_butterfly_for_group(data, len, stride, 1, 0, 1);
+    float complex rotation = (inverse > 0) ? I : -I;
     for (int stage = 2; stage < (len); stage <<= 1, rotation = csqrtf(rotation)) {
-        float complex twiddle_factor = 1;                                                    
-        for (int group = 0; group < stage; ++group, twiddle_factor *= rotation) {       
-            fast_fourier_transform_butterfly_group(data, len, stride, stage, group, twiddle_factor);                       
-        }                                                                                   
-    }                                                                                       
-    if (inverse > 0) {                                                                      
-        for (int i = 0; i < len; ++i) {                                   
+        float complex twiddle_factor = 1;
+        for (int group = 0; group < stage; ++group, twiddle_factor *= rotation) {
+            compute_butterfly_for_group(data, len, stride, stage, group, twiddle_factor);
+        }
+    }
+    if (inverse > 0) {
+        for (int i = 0; i < len; ++i) {
             data[i * stride] /= len;
-        }                                                                                   
-    }                                                                                       
+        }
+    }
 }
 
-int main() {
-    return 0;
+void fft_2d(float complex* data, int x_len, int y_len, int inverse) {
+    for (int x = 0; x < x_len; ++x) {
+        fast_fourier_transform(data + x, y_len, x_len, inverse);
+    }
+    for (int y = 0; y < y_len; ++y, data += x_len) {
+        fast_fourier_transform(data, x_len, 1, inverse);
+    }
 }
--- mfft.h
+++ mfft.h
@@ -1,2 +1,17 @@
 #pragma once
 #include <complex.h>
+
+// *** all dimentions must be a power of two ***
+#define IS_POWER_OF_TWO(X) !(X & (X - 1))
+
+void fast_fourier_transform(float complex* data, int len, int stride, int inverse);
+
+static inline void fft(float complex* data, int len) {
+    fast_fourier_transform(data, len, 1, 0);
+}
+
+static inline void ifft(float complex* data, int len) {
+    fast_fourier_transform(data, len, 1, 1);
+}
+
+void fft_2d(float complex* data, int x_len, int y_len, int inverse);
